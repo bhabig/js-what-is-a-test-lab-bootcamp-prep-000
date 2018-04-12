aaf1bd03df829a08a77d748b0be45e438b382322
Bob Habig <rfhab2@gmail.com>
2018-04-12 01:34:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-// write your code below!
-var name = "Joe"
-var height = 74
-var message = ` is ${height} inches tall`
+var name = `Susan`;https://learn.co/tracks/bootcamp-prep/javascript-fundamentals/functions-and-scope/what-is-a-test-lab#
+var height = 30
+var message = `${name} is ${height}; inches tall`;
 
 module.exports = {name, height, message}
